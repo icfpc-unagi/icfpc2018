277c96de00cf2128a3a1eeabe2205066cf89f275
Kentaro Imajo <github@imoz.jp>
2018-07-17 00:59:13 +0900

    Add API and dashbaord.

---
+++ appengine/api/public_html/time.php
@@ -0,0 +1,3 @@
+<?php
+
+echo Database::SelectCell('SELECT UNIX_TIMESTAMP()');
---
+++ appengine/dashboard/public_html/library/config.php
@@ -0,0 +1,11 @@
+<?php
+
+date_default_timezone_set('Asia/Tokyo');
+
+require_once(dirname(__FILE__) . '/database.php');
+
+Database::Initialize(
+	'master.cbr6mhuodbur.ap-northeast-1.rds.amazonaws.com', 3306,
+	'unagi', getenv('UNAGI_PASSWORD'));
+
+ini_set('memory_limit', '1G');
