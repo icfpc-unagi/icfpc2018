04e09978f8d505c7c19bde239eb6cbb110a626cc
Kentaro Imajo <github@imoz.jp>
2018-07-20 02:11:09 +0900

    Enable to reuse SSH sessions.

--- bin/unagi-upload.sh
+++ bin/unagi-upload.sh
@@ -5,6 +5,7 @@ if ! git rev-parse --is-inside-work-tree > /dev/null 2>&1; then
 	exit 1
 fi
 
+bin_directory="$(cd "$(dirname "${BASH_SOURCE}")" && pwd)"
 git_directory="$(cd "$(pwd)/$(git rev-parse --show-cdup)" && pwd)"
 
 if [ "$#" -eq 0 ]; then
@@ -16,4 +17,4 @@ fi
 
 pushd "${git_directory}" >/dev/null
 echo "Uploading '${git_directory}' to 'master:~/${target}'..."
-exec -- rsync -e unagi-ssh -a --delete --exclude target "$@" ./ "master:~/${target}"
+exec -- rsync -e "${bin_directory}/unagi-ssh" -a --delete --exclude target "$@" ./ "master:~/${target}"
