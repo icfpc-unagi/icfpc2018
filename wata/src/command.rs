b52b044095dad6c920edac4f628cbb1d1ca5e963
Takuya Akiba <t.akiba.65536@gmail.com>
2018-07-21 20:26:34 +0900

    Rustfmt

--- wata/src/command.rs
+++ wata/src/command.rs
@@ -15,30 +15,14 @@ pub enum Command {
 impl ToString for Command {
     fn to_string(&self) -> String {
         match self {
-            Command::Halt => {
-                "HALT".to_owned()
-            },
-            Command::Wait => {
-                "WAIT".to_owned()
-            },
-            Command::SMove(d) => {
-                format!("SMOVE {}", d.fmt_ld())
-            },
-            Command::LMove(d1, d2) => {
-                format!("LMOVE {} {}", d1.fmt_ld(), d2.fmt_ld())
-            },
-            Command::FusionP(p) => {
-                format!("FUSIONP {} {} {}", p.x, p.y, p.z)
-            },
-            Command::FusionS(p) => {
-                format!("FUSIONS {} {} {}", p.x, p.y, p.z)
-            },
-            Command::Fission(p, m) => {
-                format!("FISSION {} {} {} {}", p.x, p.y, p.z, m)
-            },
-            Command::Fill(p) => {
-                format!("FILL {} {} {}", p.x, p.y, p.z)
-            },
+            Command::Halt => "HALT".to_owned(),
+            Command::Wait => "WAIT".to_owned(),
+            Command::SMove(d) => format!("SMOVE {}", d.fmt_ld()),
+            Command::LMove(d1, d2) => format!("LMOVE {} {}", d1.fmt_ld(), d2.fmt_ld()),
+            Command::FusionP(p) => format!("FUSIONP {} {} {}", p.x, p.y, p.z),
+            Command::FusionS(p) => format!("FUSIONS {} {} {}", p.x, p.y, p.z),
+            Command::Fission(p, m) => format!("FISSION {} {} {} {}", p.x, p.y, p.z, m),
+            Command::Fill(p) => format!("FILL {} {} {}", p.x, p.y, p.z),
         }
     }
 }
