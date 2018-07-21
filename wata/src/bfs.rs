80949152daba08054958a3d124064187e42e47a7
Takuya Akiba <t.akiba.65536@gmail.com>
2018-07-21 20:15:17 +0900

    Rustfmt

--- wata/src/bfs.rs
+++ wata/src/bfs.rs
@@ -1,6 +1,6 @@
-use ::*;
-use std::collections::BinaryHeap;
 use std::cmp::Ordering;
+use std::collections::BinaryHeap;
+use *;
 
 /*
 DONE:
@@ -16,12 +16,12 @@ TODO items:
 */
 
 #[derive(Debug, PartialOrd, Ord, PartialEq, Eq, Clone, Copy)]
-pub struct C(i32, i32);  // (#commands, #continuous move)
+pub struct C(i32, i32); // (#commands, #continuous move)
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
 pub struct SearchState {
     p: P,
-    d: usize,  // Direction [0, 7), where 6 is a special direction
+    d: usize, // Direction [0, 7), where 6 is a special direction
 }
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
@@ -38,7 +38,7 @@ impl Ord for HeapState {
 
 impl PartialOrd for HeapState {
     fn partial_cmp(&self, other: &HeapState) -> Option<Ordering> {
-        return Some(self.c.cmp(&other.c).reverse());  // Greater, as BimaryHeap is a max-heap.
+        return Some(self.c.cmp(&other.c).reverse()); // Greater, as BimaryHeap is a max-heap.
     }
 }
 
@@ -71,45 +71,56 @@ const LIMIT_LMOVE: i32 = 5;
 const MAX_C: C = C(std::i32::MAX, std::i32::MAX);
 
 pub const DIR6: [P; 6] = [
-    P { x:  1, y: 0, z: 0 },
+    P { x: 1, y: 0, z: 0 },
     P { x: -1, y: 0, z: 0 },
-    P { x: 0, y:  1, z: 0 },
+    P { x: 0, y: 1, z: 0 },
     P { x: 0, y: -1, z: 0 },
-    P { x: 0, y: 0, z:  1 },
+    P { x: 0, y: 0, z: 1 },
     P { x: 0, y: 0, z: -1 },
 ];
 
-const DUMMY_SEARCH_STATE: SearchState = SearchState{
-    p: P{ x: -1, y: -1, z: -1},
+const DUMMY_SEARCH_STATE: SearchState = SearchState {
+    p: P {
+        x: -1,
+        y: -1,
+        z: -1,
+    },
     d: 6,
 };
 
 pub struct BFS {
     pub r: usize,
-	pub cost: V3<Vec<C>>,
+    pub cost: V3<Vec<C>>,
     pub prev: V3<Vec<SearchState>>,
     pub que: BinaryHeap<HeapState>,
     pub touched: Vec<SearchState>,
 }
 
 impl BFS {
-	pub fn new(r: usize) -> BFS {
-		BFS {
+    pub fn new(r: usize) -> BFS {
+        BFS {
             r,
-			cost: mat![MAX_C; r; r; r; 7],
+            cost: mat![MAX_C; r; r; r; 7],
             prev: mat![DUMMY_SEARCH_STATE; r; r; r; 7],
             que: BinaryHeap::new(),
             touched: vec![],
-		}
-	}
+        }
+    }
 
-    fn enqueue<G: FnMut(P) -> bool>(&mut self, next: SearchState, cost: C, prev: SearchState, filled: &mut G) {
+    fn enqueue<G: FnMut(P) -> bool>(
+        &mut self,
+        next: SearchState,
+        cost: C,
+        prev: SearchState,
+        filled: &mut G,
+    ) {
         if !next.p.is_valid(self.r) || (*filled)(next.p) {
             return;
         }
 
         if cost < self.cost[next] {
-            if self.cost[next] == MAX_C {  // First time to writing into that state?
+            if self.cost[next] == MAX_C {
+                // First time to writing into that state?
                 self.touched.push(next);
             }
 
@@ -119,16 +130,24 @@ impl BFS {
         }
     }
 
-	pub fn bfs<G: FnMut(P) -> bool, H: FnMut(P) -> bool>(
-        &mut self, mut filled: G, starts: &Vec<P>, mut goal: H
+    pub fn bfs<G: FnMut(P) -> bool, H: FnMut(P) -> bool>(
+        &mut self,
+        mut filled: G,
+        starts: &Vec<P>,
+        mut goal: H,
     ) -> Option<P> {
         // Direction 6 is a special state only for the initialization
         for &p in starts.iter() {
-            self.enqueue(SearchState{p, d: 6 }, C(0, LIMIT_SMOVE), DUMMY_SEARCH_STATE, &mut filled);
+            self.enqueue(
+                SearchState { p, d: 6 },
+                C(0, LIMIT_SMOVE),
+                DUMMY_SEARCH_STATE,
+                &mut filled,
+            );
         }
 
         while !self.que.is_empty() {
-            let HeapState{c, s} = self.que.pop().unwrap();
+            let HeapState { c, s } = self.que.pop().unwrap();
             if goal(s.p) {
                 return Some(s.p);
             }
@@ -139,29 +158,37 @@ impl BFS {
 
             // Turn: starting a new command
             for td in 0..6 {
-                self.enqueue(SearchState{p: s.p, d: td}, C(c.0 + 1, 0), s, &mut filled);
+                self.enqueue(SearchState { p: s.p, d: td }, C(c.0 + 1, 0), s, &mut filled);
             }
 
             // Turn: 2nd step of a LMove command
             if c.1 <= LIMIT_LMOVE {
                 for td in 0..6 {
-                    let ts = SearchState{p: s.p, d: td};
-                    self.enqueue(ts, C(c.0, LIMIT_SMOVE - LIMIT_LMOVE), s,  &mut filled);
+                    let ts = SearchState { p: s.p, d: td };
+                    self.enqueue(ts, C(c.0, LIMIT_SMOVE - LIMIT_LMOVE), s, &mut filled);
                 }
             }
 
             // Straight: continue
             if s.d != 6 && c.1 + 1 <= LIMIT_SMOVE {
-                self.enqueue(SearchState{p: s.p + DIR6[s.d], d: s.d}, C(c.0, c.1 + 1), prev, &mut filled);
+                self.enqueue(
+                    SearchState {
+                        p: s.p + DIR6[s.d],
+                        d: s.d,
+                    },
+                    C(c.0, c.1 + 1),
+                    prev,
+                    &mut filled,
+                );
             }
         }
 
         return None;
-	}
+    }
 
-	pub fn restore(&self, p: P) -> Vec<Command> {
-        let d: usize = (0..6).min_by_key(|&d| { self.cost[p][d] }).unwrap();
-        let mut s = SearchState{p, d};
+    pub fn restore(&self, p: P) -> Vec<Command> {
+        let d: usize = (0..6).min_by_key(|&d| self.cost[p][d]).unwrap();
+        let mut s = SearchState { p, d };
 
         let mut cmds = vec![];
 
@@ -182,10 +209,10 @@ impl BFS {
 
         cmds.reverse();
         return cmds;
-	}
+    }
 
     pub fn get_cost(&self, p: P) -> i32 {
-        return self.cost[p].iter().min().unwrap().0
+        return self.cost[p].iter().min().unwrap().0;
     }
 
     pub fn clear(&mut self) {
@@ -198,7 +225,7 @@ impl BFS {
     }
 
     pub fn show(&self) {
-        for y in 0..self.r as i32{
+        for y in 0..self.r as i32 {
             println!("[y={}]", y);
 
             for x in 0..self.r as i32 {
