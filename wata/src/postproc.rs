19684bfaf050cb0cfcdab0b5df1f8c04c8dd4214
Toshiki Kataoka <tos.lunar@gmail.com>
2018-07-22 14:23:13 +0900

    bfs clear

--- wata/src/postproc.rs
+++ wata/src/postproc.rs
@@ -17,11 +17,11 @@ pub fn fusion_all(matrix: V3<bool>, positions: Vec<P>) -> Vec<Command> {
         }
     }
     let mut cmdss: Vec<VecDeque<Command>> = Vec::new();
+    let mut bfs = bfs::BFS::new(r);
     {
         let filled_func = |p: P| { matrix[p] };
         let goal_func = |p: P| { p.x == 0 && p.y == 0 && p.z == 0 };
         for &pos in positions.iter() {
-            let mut bfs = bfs::BFS::new(r);
             let ret = bfs.bfs(filled_func, &vec![pos], goal_func);
             eprintln!("{:?} -> {:?}", pos, ret);
             if let None = ret {
@@ -30,6 +30,7 @@ pub fn fusion_all(matrix: V3<bool>, positions: Vec<P>) -> Vec<Command> {
                 }
             }
             let cmds = bfs.restore(ret.unwrap());
+            bfs.clear();
             cmdss.push(cmds.into_iter().collect());
         }
         eprintln!("{:?}", cmdss);
