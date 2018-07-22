27f15709338385a7a04d6bf9b88397cfcded97da
Kentaro Imajo <github@imoz.jp>
2018-07-23 03:56:27 +0900

    Enable to set time limit.

--- bin/execute_run.sh
+++ bin/execute_run.sh
@@ -2,6 +2,7 @@
 # unagi-upload
 
 source "$(dirname "${BASH_SOURCE}")/imosh" || exit 1
+DEFINE_bool dry_run false 'Dry run mode.'
 DEFINE_int run_id 0 'Run ID to run.'
 eval "${IMOSH_INIT}"
 
@@ -9,4 +10,4 @@ execute_run="$(cd "$(dirname "${BASH_SOURCE}")" && pwd)/execute_run.php"
 
 mkdir -p "$TMPDIR/execute_run"
 cd "$TMPDIR/execute_run"
-run_id="${FLAGS_run_id}" php "${execute_run}"
+dry_run="${FLAGS_dry_run}" run_id="${FLAGS_run_id}" php "${execute_run}"
