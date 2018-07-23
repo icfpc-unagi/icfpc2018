cfd60ccfb18995f680ed8c5d0f01301c19ccb56c
Toshiki Kataoka <tos.lunar@gmail.com>
2018-07-23 20:11:01 +0900

    a

--- wata/src/bin/print_bounding_box.rs
+++ wata/src/bin/print_bounding_box.rs
@@ -5,7 +5,10 @@ fn main() {
     let mut n = 1;
     while let Some(file) = std::env::args().nth(n) {
         let model = wata::read(&file);
-        println!("{}: {:?}", file, destruction::util::get_bounding_box(&model.filled));
+        let (l, h) = destruction::util::get_bounding_box(&model.filled);
+        println!("{}:  y(+{:2}), x({:2}+{:2}), z({:2}+{:2})", file,
+        h.y, l.x, h.x-l.x, l.z, h.z-l.z
+        );
         n += 1;
     }
 }
