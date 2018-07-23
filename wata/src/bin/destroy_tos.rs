78da12bc613615abf6da2566879bbd3c228f7677
Toshiki Kataoka <tos.lunar@gmail.com>
2018-07-23 14:56:05 +0900

    aaa

--- wata/src/bin/destroy_tos.rs
+++ wata/src/bin/destroy_tos.rs
@@ -7,6 +7,16 @@ fn main() {
     let file = std::env::args().nth(2).unwrap();
     let model = wata::read(&file);
     let r = model.r;
+    let mut y_max = 0;
+    for y in 0..r {
+        for x in 0..r {
+            for z in 0..r {
+                if model.filled[x][y][z] {
+                    y_max = y;
+                }
+            }
+        }
+    }
     let filled2 = xz::any_y(&model.filled);
     let mut filled2_fix = mat![false; r-2; r-2];
     for x in 1..r-1 {
@@ -52,19 +62,19 @@ fn main() {
             eprintln!("");
         }
 
-        let mut bot_pos = BTreeMap::new();
+        let mut bot_xz = BTreeMap::new();
         let mut orz = false;
-        for ix in 0..rx-1 {
-            for iz in 0..rz-1 {
+        for ix in 1..rx {
+            for iz in 1..rz {
                 let mut cnt = 0;
                 for a in 0..2 {
                     for b in 0..2 {
-                        let t = small[ix+a][iz+b];
+                        let t = small[ix-a][iz-b];
                         cnt += t as i32;
                         if t {
-                            bot_pos.insert(
+                            bot_xz.insert(
                                 (ix, iz),
-                                P::new((bx[ix+1]+a-1) as i32, 0, (bz[iz+1]+b-1) as i32));
+                                P::new((bx[ix]-a) as i32, 0, (bz[iz]-b) as i32));
                         }
                     }
                 }
@@ -73,10 +83,103 @@ fn main() {
                 }
             }
         }
-        if orz || bot_pos.len() > 20 {
+        if orz || bot_xz.len() > 20 {
             continue;
         }
-        eprintln!("ok");
+        eprintln!("ok {:?}", bot_xz);
+
+        {
+            let xz_set: BTreeSet<_> = bot_xz.values().cloned().collect();
+            if xz_set.len() < bot_xz.len() {
+                continue;
+            }
+        }
+
+        let mut y_high = 1.max(y_max as i32);
+        let mut y_low = 0.max(y_high - 30 + 1);
+        let mut bot_ps = Vec::new();
+        bot_ps.append(&mut bot_xz.values().map(|&xz: &P| xz + P::new(0, y_low, 0)).collect());
+        bot_ps.append(&mut bot_xz.values().map(|&xz: &P| xz + P::new(0, y_high, 0)).collect());
+
+        let (bids, fission_cmds) = fission_to(&model.filled, &bot_ps);
+        // let mut bids_low = BTreeMap::from_iter(bot_xz.keys().zip(bids[..bot_xz.len()].iter()));
+        let mut bids_low = BTreeMap::new();
+        for (ixz, bid) in bot_xz.keys().zip(bids[..bot_xz.len()].iter()) {
+            bids_low.insert(ixz, bid);
+        }
+        let mut bids_high = BTreeMap::new();
+        for (ixz, bid) in bot_xz.keys().zip(bids[bot_xz.len()..].iter()) {
+            bids_high.insert(ixz, bid);
+        }
+        eprintln!("{:?}", bids);
+        eprintln!("{:?}", bids_low);
+        eprintln!("{:?}", bids_high);
+
+        let mut sorted_bids = bids.clone();
+        sorted_bids.sort();
+        let sorted_bids = sorted_bids;
+
+        let mut main_cmds = Vec::new();
+        {
+            let mut cmds = Vec::new();
+            cmds.push(Command::Flip);
+            for _ in 1..bids.len() {
+                cmds.push(Command::Wait);
+            }
+            main_cmds.append(&mut cmds);
+        }
+        loop {
+            for ix in 1..rx {
+                for iz in 1..rz {
+                    if small[ix][iz] {
+                        let bx2 = [bx[ix] as i32, bx[ix+1] as i32];
+                        // let by2 = [y_low, y_high];
+                        let bz2 = [bz[iz] as i32, bz[iz+1] as i32];
+                        let mut cmds = BTreeMap::new();
+                        for a in 0..2 {
+                            for b in 0..2 {
+                                let bid = bids_low[&(ix+a, iz+b)];
+                                let nd = P::new(bx2[a], 0, bz2[b]) - bot_xz[&(ix, iz)];
+                                let fd = P::new(bx2[1-a] - bx2[a], y_high - y_low, bz2[1-b] - bz2[b]);
+                                cmds.insert(bid, Command::GFill(nd, fd));
+
+                                let bid = bids_high[&(ix+a, iz+b)];
+                                let nd = P::new(bx2[a], 0, bz2[b]) - bot_xz[&(ix, iz)];
+                                let fd = P::new(bx2[1-a] - bx2[a], y_low - y_high, bz2[1-b] - bz2[b]);
+                                cmds.insert(bid, Command::GFill(nd, fd));
+                            }
+                        }
+                        eprintln!("{:?}", cmds);
+                        let mut cmds = sorted_bids.iter().map(|bid| cmds.get(&bid).unwrap_or(&Command::Wait)).cloned().collect();
+                        eprintln!("{:?}", cmds);
+                        main_cmds.append(&mut cmds);
+                    }
+                }
+            }
+            if y_low == 0 {
+                break;
+            }
+            let new_y_low = 0.max(y_low - 30);
+            let mut y_down = y_low - new_y_low;
+            y_low -= y_down;
+            y_high -= y_down;
+            while y_down > 0 {
+                let dy = -(15.min(y_down));
+                let mut cmds = vec![Command::SMove(P::new(0, dy, 0)); sorted_bids.len()];
+                main_cmds.append(&mut cmds);
+                y_down += dy;
+            }
+        }
+
+        let mut positions = BTreeMap::new();
+        for ixz in bot_xz.keys() {
+            positions.insert(bids_low[ixz], bot_xz[ixz] + P::new(0, y_low, 0));
+            positions.insert(bids_high[ixz], bot_xz[ixz] + P::new(0, y_high, 0));
+        }
+        let positions = positions.values().cloned().collect();
+        let fusion_cmds = postproc::fusion_all(&mat![false; r; r; r], positions);
+        eprintln!("{} {} {}", fission_cmds.len(), main_cmds.len(), fusion_cmds.len());
+        return;
     }
 }
 
